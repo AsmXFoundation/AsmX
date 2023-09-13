3c8161be7a2cdfcf8e2795ca1dc94e574dab60f2
E5war5IT <eduard.17072006@gmail.com>
2023-09-13 07:37:18 +0300

    :sparkles: `variable & constant`

--- etc/os/AsmXOS/usr/bin/asmx/index.js
+++ etc/os/AsmXOS/usr/bin/asmx/index.js
@@ -8,13 +8,49 @@ class AsmXShell {
 
         for (const line of content_t) {
             let response;
-            if (!line.startsWith('#')) response = cli.execute(line.split(' '));
-            
-            if (typeof response == 'string') console.log(response);
+            let cmds = line.split(' ');
 
-            else if (typeof response == 'object' && !Array.isArray(response)) {
-                if (response?.type && response.type == 'thread') {
-                    for (const answer_t of response.response) console.log(answer_t);
+            if ([1, 2].includes(cmds.indexOf('='))) {
+                let token = cmds.indexOf('=') == 2 ? cmds[0] : false;
+                let name = cmds[0];
+
+                if (token && ['let', 'const'].includes(token)) {
+                    name = cmds[1];
+                    let value = cmds.slice(cmds.indexOf('=') + 1).join(' ');
+
+                    if (token == 'let') {
+                        cli.userVariable[name] = value;
+                    } else if (token == 'const') {
+                        if (!Reflect.ownKeys(cli.userConstant).includes(name)) cli.userConstant[name] = value;
+                    }
+                } else if (!token) {
+                    cli.userVariable[name] = cmds.slice(cmds.indexOf('=') + 1).join(' ');
+                }
+            } else {
+                let countVariables = Reflect.ownKeys(cli.userVariable).length + Reflect.ownKeys(cli.userConstant).length;
+
+                if (countVariables > 0) {
+                    cmds.forEach((cmd, i) => {
+                        if (/\%[a-zA-Z][a-zA-Z0-9_]+\%/.exec(cmd)) {
+                            let name = /\%([a-zA-Z][a-zA-Z0-9_]+)\%/.exec(cmd)[1].trim();
+
+                            if (Reflect.ownKeys(cli.userVariable).includes(name)) {
+                                cmds[i] = cmd.replaceAll(`%${name}%`, cli.userVariable[name]);
+                            } else if (Reflect.ownKeys(cli.userConstant).includes(name)) {
+                                cmds[i] = cmd.replaceAll(`%${name}%`, cli.userConstant[name]);
+                            }
+                        }
+                    });
+                }
+
+                if (!line.startsWith('#')) response = cli.execute(cmds);
+                
+                if (typeof response == 'string') console.log(response);
+    
+                else if (typeof response == 'object' && !Array.isArray(response)) {
+                    if (response?.type && response.type == 'thread') {
+                        for (const answer_t of response.response) console.log(answer_t);
+                    }
                 }
             }
         }
---
+++ etc/os/AsmXOS/usr/var.ash
@@ -0,0 +1,5 @@
+let user = Elon Musk!
+id = 1
+const place = Texas
+
+print Hi %user% %user% place: %place% id: %id%
