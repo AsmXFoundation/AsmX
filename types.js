d27a6cb22ff4edbc231b6ebba20e112c6728e2e1
E5war5IT <eduard.17072006@gmail.com>
2023-09-18 19:25:56 +0300

    :memo: `updates files`

--- types.js
+++ types.js
@@ -1,5 +1,3 @@
-const { ArgumentError } = require("./exception");
-
 class Type {
     static types = [];
 
@@ -57,51 +55,6 @@ class Type {
 }
 
 
-class List {
-    constructor(...items) {
-        items.forEach(item => {
-            if (!this.checkType(item)) {
-                process.stdout.write(ArgumentError.ARGUMENT_INVALID_TYPE_ARGUMENT);
-                process.exit();
-            }
-        });
-        
-        this.items = items;
-        this.writable = true;
-        this.readable = true;
-        this.type = 'List';
-    }
-
-    checkType(item) {
-        if (item === undefined || item === null) return true;
-        if (Array.isArray(item)) return item.every(subItem => this.checkType(subItem));
-        return item.constructor.name === this.type;
-    }
-
-    toList(){
-        return this.items.slice();
-    }
-
-    get(index){
-        return this.items[index];
-    }
-
-    [Symbol.hasInstance](instance) {
-        return instance instanceof List;
-    }
-}
-
-
-class Tuple extends List {
-    constructor(...items) {
-        super(...items);
-        this.writable = false;
-        this.readable = false;
-        this.type = 'Tuple';
-    }
-}
-
-
 Type.new('String', /'[^"]*'/);
 Type.new('String', /"[^']*"/);
 Type.new('Int', /(^[+-]?\d+$)/);
@@ -117,7 +70,5 @@ Type.new('hex', /^0[xX][0-9a-fA-F]+/);
 Type.new('bool', /true|false/);
 
 module.exports = {
-    Type,
-    Tuple,
-    List
+    Type
 }
