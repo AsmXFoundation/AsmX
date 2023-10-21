97baf5949c3c6041960daf9413745c48188139af
E5war5IT <91418370+E5war5IT@users.noreply.github.com>
2023-10-22 00:04:18 +0300

    :memo: `update source`

--- etc/os/AsmXOS/usr/packages/url/index.js
+++ etc/os/AsmXOS/usr/packages/url/index.js
@@ -3,7 +3,13 @@ const ServerLog = require('../../../../../../server/log');
 class CLI {
     static url() {
         let parameters = this.cli_args.slice(1);
-        let [flags, url, last] = [['--parse', '--params'],, parameters[parameters.length -1]];
+        let [flags, url, last, isPerf] = [['--parse', '--params'],, parameters[parameters.length - 1], false];
+
+        if (last == '--perf') {
+            parameters.pop();
+            last = parameters[parameters.length - 1];
+            isPerf = true;
+        }
 
         if (flags.includes(last)) {
             url = parameters.slice(0, -1).join('');
@@ -16,6 +22,7 @@ class CLI {
             if (last == '--parse') {
                 try {
                     let response = '{\n';
+                    let startPerfomance = performance.now();
                     url = new URL(url);
 
                     let properties = ['href', 'origin', 'protocol', 'username', 'password', 'host', 'hostname', 'port', 'pathname', 'search', 'hash'];
@@ -24,6 +31,9 @@ class CLI {
                     response = response.trimEnd().slice(0, -1);
                     response += '\n}';
 
+                    let endPerfomance = performance.now();
+                    isPerf && console.log(`[time]: ${(endPerfomance - startPerfomance).toFixed(2)}ms`);
+
                     return response;
                 } catch {
                     ServerLog.log('Path parsing error\n', 'Exception');
@@ -31,12 +41,15 @@ class CLI {
             } else if (last == '--params') {
                 try {
                     let response = '{\n';
-
+                    let startPerfomance = performance.now();
                     for (const [key, value] of new URLSearchParams(url).entries())response += ` ${key}: '${value}',\n`;
                     
                     response = response.trimEnd().slice(0, -1);
                     response += '\n}';
                     
+                    let endPerfomance = performance.now();
+                    isPerf && console.log(`[time]: ${(endPerfomance - startPerfomance).toFixed(2)}ms`);
+
                     return response;
                 } catch {
                     ServerLog.log('Path parsing error\n', 'Exception');
